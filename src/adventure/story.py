91cbce1fc007a6a803cfc10fd5f2bfd056bcc68d
Austin Miller <156855264+aujmiller@users.noreply.github.com>
2025-10-28 13:49:58 -0400

    Format Story Text (#2)
    * Modified outputs in story.py to be color coded.
    
    * Made 'Which direction...' output text bold

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,7 +1,9 @@
 from adventure.utils import read_events_from_file
 import random
+from rich import print
+from rich.console import Console
 
-default_message = "You stand still, unsure what to do. The forest swallows you."
+default_message = "[bright_green]You stand still, unsure what to do. The forest swallows you.[/bright_green]"
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -14,17 +16,18 @@ def step(choice: str, events):
         return default_message
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[bright_yellow]You walk [bright_blue]left[/bright_blue]. " + event + "[/bright_yellow]"
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[bright_yellow]You walk [bright_red]right[/bright_red]. " + event + "[/bright_yellow]"
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
+    console = Console()
 
-    print("You wake up in a dark forest. You can go left or right.")
+    print("[bright_green]You wake up in a dark forest. You can go [bright_blue]left[/bright_blue] or [bright_red]right[/bright_red].[/bright_green]")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        choice = console.input("[bright_green][bold]Which direction do you choose? ([bright_blue]left[/bright_blue]/[bright_red]right[/bright_red]/[bright_magenta]exit[/bright_magenta]): [/bold][/bright_green]")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
