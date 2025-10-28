4966faaa56d4dc48c230d883b324d0aa2d6fa369
Austin Miller <156855264+aujmiller@users.noreply.github.com>
2025-10-28 17:44:59 +0000

    Made 'Which direction...' output text bold

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
     print("[bright_green]You wake up in a dark forest. You can go [bright_blue]left[/bright_blue] or [bright_red]right[/bright_red].[/bright_green]")
     while True:
-        choice = console.input("[bright_green]Which direction do you choose? ([bright_blue]left[/bright_blue]/[bright_red]right[/bright_red]/[bright_magenta]exit[/bright_magenta]): [/bright_green]")
+        choice = console.input("[bright_green][bold]Which direction do you choose? ([bright_blue]left[/bright_blue]/[bright_red]right[/bright_red]/[bright_magenta]exit[/bright_magenta]): [/bold][/bright_green]")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
